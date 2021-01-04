98e659337ff0ef8c0471ba94a8b20585401c0dae
luannh <huuluan0201@gmail.com>
2021-01-04 13:20:01 +0700

    change command

--- src/ckfindercommand.js
+++ src/ckfindercommand.js
@@ -9,8 +9,8 @@
  * @module ckfinder/ckfindercommand
  */
 
-import Command from '@ckeditor/ckeditor5-core/src/command';
-import CKEditorError from '@ckeditor/ckeditor5-utils/src/ckeditorerror';
+import Command from "@ckeditor/ckeditor5-core/src/command";
+import CKEditorError from "@ckeditor/ckeditor5-utils/src/ckeditorerror";
 
 /**
  * The CKFinder command. It is used by the {@link module:ckfinder/ckfinderediting~CKFinderEditing CKFinder editing feature}
@@ -30,22 +30,30 @@ export default class CKFinderCommand extends Command {
 	/**
 	 * @inheritDoc
 	 */
-	constructor( editor ) {
-		super( editor );
+	constructor(editor) {
+		super(editor);
 
 		// Remove default document listener to lower its priority.
-		this.stopListening( this.editor.model.document, 'change' );
+		this.stopListening(this.editor.model.document, "change");
 
 		// Lower this command listener priority to be sure that refresh() will be called after link & image refresh.
-		this.listenTo( this.editor.model.document, 'change', () => this.refresh(), { priority: 'low' } );
+		this.listenTo(
+			this.editor.model.document,
+			"change",
+			() => this.refresh(),
+			{ priority: "low" }
+		);
+
+		// Change config
+		this.editor.commands.set("command", "GetFileInfo");
 	}
 
 	/**
 	 * @inheritDoc
 	 */
 	refresh() {
-		const imageCommand = this.editor.commands.get( 'imageInsert' );
-		const linkCommand = this.editor.commands.get( 'link' );
+		const imageCommand = this.editor.commands.get("imageInsert");
+		const linkCommand = this.editor.commands.get("link");
 
 		// The CKFinder command is enabled when one of image or link command is enabled.
 		this.isEnabled = imageCommand.isEnabled || linkCommand.isEnabled;
@@ -57,13 +65,17 @@ export default class CKFinderCommand extends Command {
 	execute() {
 		const editor = this.editor;
 
-		const openerMethod = this.editor.config.get( 'ckfinder.openerMethod' ) || 'modal';
+		const openerMethod =
+			this.editor.config.get("ckfinder.openerMethod") || "modal";
 
-		if ( openerMethod != 'popup' && openerMethod != 'modal' ) {
-			throw new CKEditorError( 'ckfinder-unknown-openerMethod: The openerMethod config option must by "popup" or "modal".', editor );
+		if (openerMethod != "popup" && openerMethod != "modal") {
+			throw new CKEditorError(
+				'ckfinder-unknown-openerMethod: The openerMethod config option must by "popup" or "modal".',
+				editor
+			);
 		}
 
-		const options = this.editor.config.get( 'ckfinder.options' ) || {};
+		const options = this.editor.config.get("ckfinder.options") || {};
 
 		options.chooseFiles = true;
 
@@ -71,79 +83,91 @@ export default class CKFinderCommand extends Command {
 		const originalOnInit = options.onInit;
 
 		// Pass the lang code to the CKFinder if not defined by user.
-		if ( !options.language ) {
+		if (!options.language) {
 			options.language = editor.locale.uiLanguage;
 		}
 
 		// The onInit method allows to extend CKFinder's behavior. It is used to attach event listeners to file choosing related events.
-		options.onInit = finder => {
+		options.onInit = (finder) => {
 			// Call original options.onInit if it was defined by user.
-			if ( originalOnInit ) {
-				originalOnInit( finder );
+			if (originalOnInit) {
+				originalOnInit(finder);
 			}
 
-			finder.on( 'files:choose', evt => {
+			finder.on("files:choose", (evt) => {
 				const files = evt.data.files.toArray();
 
 				// Insert links
-				const links = files.filter( file => !file.isImage() );
-				const images = files.filter( file => file.isImage() );
+				const links = files.filter((file) => !file.isImage());
+				const images = files.filter((file) => file.isImage());
 
-				for ( const linkFile of links ) {
-					editor.execute( 'link', linkFile.getUrl() );
+				for (const linkFile of links) {
+					editor.execute("link", linkFile.getUrl());
 				}
 
 				const imagesUrls = [];
 
-				for ( const image of images ) {
+				for (const image of images) {
 					const url = image.getUrl();
 
-					imagesUrls.push( url ? url : finder.request( 'file:getProxyUrl', { file: image } ) );
+					imagesUrls.push(
+						url
+							? url
+							: finder.request("file:getProxyUrl", {
+									file: image,
+							  })
+					);
 				}
 
-				if ( imagesUrls.length ) {
-					insertImages( editor, imagesUrls );
+				if (imagesUrls.length) {
+					insertImages(editor, imagesUrls);
 				}
-			} );
+			});
 
-			finder.on( 'file:choose:resizedImage', evt => {
+			finder.on("file:choose:resizedImage", (evt) => {
 				const resizedUrl = evt.data.resizedUrl;
 
-				if ( !resizedUrl ) {
-					const notification = editor.plugins.get( 'Notification' );
+				if (!resizedUrl) {
+					const notification = editor.plugins.get("Notification");
 					const t = editor.locale.t;
 
-					notification.showWarning( t( 'Could not obtain resized image URL.' ), {
-						title: t( 'Selecting resized image failed' ),
-						namespace: 'ckfinder'
-					} );
+					notification.showWarning(
+						t("Could not obtain resized image URL."),
+						{
+							title: t("Selecting resized image failed"),
+							namespace: "ckfinder",
+						}
+					);
 
 					return;
 				}
 
-				insertImages( editor, [ resizedUrl ] );
-			} );
+				insertImages(editor, [resizedUrl]);
+			});
 		};
 
-		window.CKFinder[ openerMethod ]( options );
+		window.CKFinder[openerMethod](options);
 	}
 }
 
-function insertImages( editor, urls ) {
-	const imageCommand = editor.commands.get( 'imageInsert' );
+function insertImages(editor, urls) {
+	const imageCommand = editor.commands.get("imageInsert");
 
 	// Check if inserting an image is actually possible - it might be possible to only insert a link.
-	if ( !imageCommand.isEnabled ) {
-		const notification = editor.plugins.get( 'Notification' );
+	if (!imageCommand.isEnabled) {
+		const notification = editor.plugins.get("Notification");
 		const t = editor.locale.t;
 
-		notification.showWarning( t( 'Could not insert image at the current position.' ), {
-			title: t( 'Inserting image failed' ),
-			namespace: 'ckfinder'
-		} );
+		notification.showWarning(
+			t("Could not insert image at the current position."),
+			{
+				title: t("Inserting image failed"),
+				namespace: "ckfinder",
+			}
+		);
 
 		return;
 	}
 
-	editor.execute( 'imageInsert', { source: urls } );
+	editor.execute("imageInsert", { source: urls });
 }
